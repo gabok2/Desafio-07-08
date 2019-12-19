007a41cc19b36eaf168f4d4ae2f5c25a7d33a421
gabriel <53623479+gabok2@users.noreply.github.com>
2019-12-18 21:16:17 -0300

    Components globais

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -20,6 +20,7 @@ function Header({ cartSize }) {
           <strong>Meu carrinho</strong>
           <span> {cartSize} itens</span>
         </div>
+
         <MdShoppingBasket size={36} color="#fff" />
       </Cart>
     </Container>
