64b36a3be7648d278e80cec1d97228385c5c553a
gabriel <53623479+gabok2@users.noreply.github.com>
2019-12-18 21:21:26 -0300

    Redux

--- src/store/modules/cart/redurcer.js
+++ src/store/modules/cart/redurcer.js
@@ -20,6 +20,7 @@ export default function cart(state = [], action) {
     case '@cart/UPDATE_AMOUNT_SUCCESS': {
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.id);
+
         if (productIndex >= 0) {
           draft[productIndex].amount = Number(action.amount);
         }
